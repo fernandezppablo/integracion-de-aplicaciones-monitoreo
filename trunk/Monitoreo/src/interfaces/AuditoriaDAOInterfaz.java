d4fce18234e39cb7912d95d391da35648a446ef5
marinojuanfrancisco@gmail.com <marinojuanfrancisco@gmail.com@1fa5aaa0-229c-b706-55a0-73166ed91dc4>
2014-10-14 16:34:25 +0000

    Agregué el servicio RSMonitoreo

--- trunk/Monitoreo/src/interfaces/AuditoriaDAOInterfaz.java
+++ trunk/Monitoreo/src/interfaces/AuditoriaDAOInterfaz.java
@@ -1,5 +1,8 @@
 package interfaces;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -13,5 +16,5 @@ public interface AuditoriaDAOInterfaz {
 	public void grabarAuditoria(Auditoria aGrabar);
 	public void grabarVenta(TROrdenVenta trov);
 	public Articulo traerArticuloPorCodigo(Integer id);
-	
+	public List<Map<Integer,Integer>> rankingArticulos();
 }
